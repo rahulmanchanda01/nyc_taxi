754d6ef709f04cab012d721ed74a1feb59489a29
surag17 <surag0107@live.com>
2016-08-07 17:28:29 -0500

    Lulz

--- nyc_taxis.py
+++ nyc_taxis.py
@@ -2,28 +2,42 @@
 """
 Created on Sat Jul 30 03:20:29 2016
 
-@author: SuryaNarayana
+@author: Surag
 """
 from pandas import Series, DataFrame
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
+import os
 
-nlinesfile = 11460573
-nlinesrandomsample = 50000
-lines2skip = np.random.choice(np.arange(1,nlinesfile+1), \
-(nlinesfile-nlinesrandomsample), replace=False)
-np.random.seed()
+np.random.seed(456)
+nlinesrandomsample = 100000
 
-nyc_taxi_12 = pd.read_csv('yellow_tripdata_2015-12.csv',skiprows=lines2skip)    
-nyc_taxi_11 = pd.read_csv('yellow_tripdata_2015-11.csv',skiprows=lines2skip) 
-nyc_taxi_10 = pd.read_csv('yellow_tripdata_2015-10.csv',skiprows=lines2skip) 
-nyc_taxi_09 = pd.read_csv('yellow_tripdata_2015-09.csv',skiprows=lines2skip) 
-nyc_taxi_08 = pd.read_csv('yellow_tripdata_2015-08.csv',skiprows=lines2skip) 
-nyc_taxi_07 = pd.read_csv('yellow_tripdata_2015-07.csv',skiprows=lines2skip) 
-nyc_taxi_06 = pd.read_csv('yellow_tripdata_2015-06.csv',skiprows=lines2skip) 
+nlines = list([13158262,12324935,11562783,11130304,11225063,12315488,11312676,1048575])
+files = os.listdir('Datasets')
 
+nyc_taxi_may_dec2015 = pd.DataFrame()
+
+for i in range(8):
+    lines2skip = np.random.choice(np.arange(1,nlines[i]+1), \
+(nlines[i]-nlinesrandomsample), replace=False)
+    nyc_taxi_temp = pd.read_csv('Datasets/'+files[i],skiprows = lines2skip)
+    nyc_taxi_may_dec2015 = nyc_taxi_may_dec2015.append(nyc_taxi_temp)
+    del nyc_taxi_temp
+    
+
+#==============================================================================
+# nyc_taxi_12 = pd.read_csv('Datasets/yellow_tripdata_2015-12.csv')
+# nyc_taxi_11 = pd.read_csv('Datasets/yellow_tripdata_2015-11.csv')
+# nyc_taxi_10 = pd.read_csv('Datasets/yellow_tripdata_2015-10.csv') 
+# 
+# nyc_taxi_09 = pd.read_csv('Datasets/yellow_tripdata_2015-09.csv') 
+# nyc_taxi_08 = pd.read_csv('Datasets/yellow_tripdata_2015-08.csv') 
+# nyc_taxi_07 = pd.read_csv('Datasets/yellow_tripdata_2015-07.csv') 
+# nyc_taxi_06 = pd.read_csv('Datasets/yellow_tripdata_2015-06.csv') 
+# nyc_taxi_05 = pd.read_csv('Datasets/yellow_tripdata_2015-05.csv') 
+#==============================================================================
 #pickup_loc = np.array([nyc_taxi_12['pickup_longitude'],nyc_taxi_12['pickup_latitude']]) 
 #
 #fig = plt.figure()
