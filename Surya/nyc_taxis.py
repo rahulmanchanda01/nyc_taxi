fbf85ccdffa8fe63bd68be8ab36036ad59560462
suryak1210 <surya.kalipattapu@gmail.com>
2016-08-08 11:13:25 -0500

    Added code for plotting on plotly

--- Surya/nyc_taxis.py
+++ Surya/nyc_taxis.py
@@ -13,6 +13,10 @@ import os
 from geopy.geocoders import Nominatim
 import gmplot
 import geocoder 
+from uszipcode import ZipcodeSearchEngine
+search = ZipcodeSearchEngine()
+import plotly.plotly as py
+py.sign_in('suryak1210', 'nd72ait1tg')
 
 geolocator = Nominatim()
 
@@ -22,11 +26,15 @@ geolocator = Nominatim()
 #    lctn_zip = int(location.raw['address']['postcode'][0:5])
 #    return lctn_zip
 
+#def cordnts_zip(a,b):
+#    location = geocoder.google([a,b],method ="reverse")
+#    lctn_zip = location.postal
+#    return lctn_zip
+
 def cordnts_zip(a,b):
-    location = geocoder.google([a,b],method ="reverse")
-    lctn_zip = location.postal
-    return lctn_zip
-    
+    location = search.by_coordinate(a,b)
+    lctn_zip = location[0]["Zipcode"]
+    return lctn_zip    
     
 np.random.seed(456)
 nlinesrandomsample = 100000
@@ -54,8 +62,16 @@ nyc_taxi_may_dec2015 = nyc_taxi_may_dec2015.set_index(np.arange(0,800000))
 nyc_taxi_may_dec2015 = nyc_taxi_may_dec2015[(nyc_taxi_may_dec2015['pickup_latitude'] != 0 ) \
                         & (nyc_taxi_may_dec2015['pickup_longitude']!=0 )]
 
-nyc_taxi_may_dec2015['location_zip'] = nyc_taxi_may_dec2015.apply(lambda x : 
-    cordnts_zip(x['pickup_latitude'],x['pickup_longitude']),axis=1)
+nyc_taxi_may_dec2015['dropoff_location_zip'] = nyc_taxi_may_dec2015.apply(lambda x : 
+    cordnts_zip(x['dropoff_latitude'],x['dropoff_longitude']),axis=1)
+
+nyc_taxi_trial = nyc_taxi_may_dec2015.head(1)
+
+### Merging zip codes data set
+zip_codes = pd.read_csv('zips.csv')
+nyc_taxi_may_dec2015_zip = pd.merge(nyc_taxi_may_dec2015,zip_codes,\
+ left_on=['pickup_latitude','pickup_longitude'], \
+ right_on=['latitude','longitude] )
 
 ### Plottong pickup locations
      
@@ -77,30 +93,58 @@ rcParams['figure.figsize'] = (17.5, 17) #Size of figure
 rcParams['figure.dpi'] = 250
 
 
-P=nyc_taxi_may_dec2015.plot(kind='scatter', x='dropoff_longitude', y='dropoff_latitude',color='white',\
+P=nyc_taxi_may_dec2015.plot(kind='scatter', x='dropoff_longitude', \
+        y='dropoff_latitude',color='white',\
         xlim=(-74.06,-73.77),ylim=(40.61, 40.91),s=.02,alpha=.6)
 P.set_axis_bgcolor('#800000') #Background Color
 
-#m = Basemap(projection='mill',llcrnrlat=20,urcrnrlat=50,\
-#            llcrnrlon=-130,urcrnrlon=-60,resolution='c')
-#fig = plt.figure()
-#themap = Basemap(projection='gall',llcrnrlat=35.,urcrnrlat=50,\
-#                llcrnrlon=-130,urcrnrlon=-60,resolution='l')
+a = list(nyc_taxi_may_dec2015['pickup_latitude'].head(10000))
+b = list(nyc_taxi_may_dec2015['pickup_longitude'].head(10000))
+#### Scatter plot
 
-#themap.drawcoastlines()
-#themap.drawcountries()
-#themap.fillcontinents(color = 'yellow')
-#themap.drawmapboundary(fill_color='blue')
-#
-#a = nyc_taxi_may_dec2015['pickup_longitude'].head(25)
-#b = nyc_taxi_may_dec2015['pickup_latitude'].head(25)
-#
-#for x1,y1 in zip(a,b):
-#    x, y = themap(x1,y1)               
-#    themap.plot(x, y, 
-#                'o',                    # marker shape
-#                color='red',         # marker colour
-#                markersize=5           # marker size
-#                )
-#    
-#plt.show()
+scl = [ [0,"rgb(5, 10, 172)"],[0.35,"rgb(40, 60, 190)"],[0.5,"rgb(70, 100, 245)"],\
+    [0.6,"rgb(90, 120, 245)"],[0.7,"rgb(106, 137, 247)"],[1,"rgb(220, 220, 220)"] ]
+
+data = [ dict(
+        type = 'scattergeo',
+        locationmode = 'USA-states',
+        lon = nyc_taxi_may_dec2015['pickup_longitude'].head(10000) ,
+        lat = nyc_taxi_may_dec2015['pickup_latitude'].head(10000),
+#        text = df['text'],
+        mode = 'markers',
+        marker = dict( 
+            size = 8, 
+            opacity = 0.8,
+            reversescale = True,
+            autocolorscale = False,
+            symbol = 'square',
+            line = dict(
+                width=1,
+                color='rgba(102, 102, 102)'
+            ),
+            colorscale = scl,
+            cmin = 0,
+            color = df['cnt'],
+            cmax = df['cnt'].max(),
+            colorbar=dict(
+                title="Pickup Location"
+            )
+        ))]
+
+layout = dict(
+        title = 'NYC TAXI',
+        colorbar = True,   
+        geo = dict(
+            scope='NY',
+            projection=dict( type='albers usa' ),
+            showland = True,
+            landcolor = "rgb(250, 250, 250)",
+            subunitcolor = "rgb(217, 217, 217)",
+            countrycolor = "rgb(217, 217, 217)",
+            countrywidth = 0.5,
+            subunitwidth = 0.5        
+        ),
+    )
+
+fig = dict( data=data, layout=layout )
+py.iplot( fig, validate=False)
